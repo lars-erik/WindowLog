67519c7a74729ecd7879da539f3c89476b634055
Lars-Erik <lea@markedspartner.no>
2022-09-19 22:54:43 +0200

    Back to restarting for aggregate total updates for now. 🤷‍♂️

--- WindowLog.GUI/DurationTotalConverter.cs
+++ WindowLog.GUI/DurationTotalConverter.cs
@@ -9,7 +9,45 @@ using System.Windows.Data;
 
 namespace WindowLog.GUI
 {
-    public class DurationTotalConverter : IMultiValueConverter
+    public class DurationTotalConverter : IValueConverter
+    {
+        private const string FormatString = @"d\.hh\:mm\:ss";
+        private static readonly string Blank = TimeSpan.Zero.ToString(FormatString);
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value is ICollection<object> coll)
+                {
+                    if (coll.FirstOrDefault() is EntryModel)
+                    {
+                        var retVal = coll.Cast<EntryModel>()
+                            .Aggregate(TimeSpan.Zero, (c, e) => c + (e.Duration ?? TimeSpan.Zero));
+                        return retVal;
+                    }
+
+                    if (coll.FirstOrDefault() is CollectionViewGroup)
+                    {
+                        var retVal = coll.Cast<CollectionViewGroup>().Aggregate(TimeSpan.Zero,
+                            (span, item) => span + item.Items.Cast<EntryModel>().Aggregate(TimeSpan.Zero, (c, e) => c + (e.Duration ?? TimeSpan.Zero)));
+                        return retVal;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            return TimeSpan.Zero;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new []{ value };
+        }
+    }
+    public class DurationMultiConverter : IMultiValueConverter
     {
         private const string FormatString = @"d\.hh\:mm\:ss";
         private static readonly string Blank = TimeSpan.Zero.ToString(FormatString);
